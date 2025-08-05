563da0b1b8590e818866ea5ec3bfa02684302176
ISBRATT <br14tty@outlook.com>
2025-08-05 19:48:08 +0100

    Remove Num9 button functionality in Form1.cs
    This commit modifies the `Num9_Click` event handler in the
    `CalculatorApp` namespace by removing the line that adds "8"
    to the `ResultsBox`. This change alters the functionality
    associated with the Num9 button.
    
    Refactor calculator input handling and operations
    
    - Clear `ResultsBox` before displaying user input for numbers.
    - Update `EqualButton_Click` to perform basic arithmetic operations.
    - Modify `ClearButton_Click` to reset input variables and display.
    - Store user input in `first` variable in operation buttons.
    - Add `using` directive for `System.Diagnostics.Eventing.Reader`.

--- CalculatorApp/Form1.cs
+++ CalculatorApp/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,47 +26,64 @@ namespace CalculatorApp
 
         private void Num1_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "1";
+            ResultsBox.Text = "";
+            userInput += "1";
+            ResultsBox.Text += userInput;
         }
 
         private void Num2_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "2";
+            ResultsBox.Text = "";
+            userInput += "2";
+            ResultsBox.Text += userInput;
         }
 
         private void Num3_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "3";
+            ResultsBox.Text = "";
+            userInput += "3";
+            ResultsBox.Text += userInput;
         }
 
         private void Num4_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "4";
+            ResultsBox.Text = "";
+            userInput += "4";
+            ResultsBox.Text += userInput;
         }
 
         private void Num5_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "5";
+            ResultsBox.Text = "";
+            userInput += "5";
+            ResultsBox.Text += userInput;
         }
 
         private void Num6_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "6";
+            ResultsBox.Text = "";
+            userInput += "6";
+            ResultsBox.Text += userInput;
         }
-
         private void Num7_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "7";
+            ResultsBox.Text = "";
+            userInput += "7";
+            ResultsBox.Text += userInput;
         }
 
         private void Num8_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "8";
+            ResultsBox.Text = "";
+            userInput += "8";
+            ResultsBox.Text += userInput;
         }
 
         private void ZeroButton_Click(object sender, EventArgs e)
         {
-            ResultsBox.Text += "0";
+            ResultsBox.Text = "";
+            userInput += "0";
+            ResultsBox.Text += userInput;
         }
 
         private void DecimalButton_Click(object sender, EventArgs e)
@@ -75,37 +93,89 @@ namespace CalculatorApp
 
         private void EqualButton_Click(object sender, EventArgs e)
         {
-            function = '=';
+            second = userInput;
+            double firstNum, secondNum;
+            firstNum = Convert.ToDouble(first);
+            secondNum = Convert.ToDouble(second);
+            //Plus
+            if(function == '+')
+            {
+                result = firstNum + secondNum;
+                ResultsBox.Text = result.ToString();
+            }
+            //Minus
+            else if (function == '-')
+            {
+                result = firstNum - secondNum;
+                ResultsBox.Text = result.ToString();
+            }
+            //Divide
+            else if (function == '/')
+            {
+                if (secondNum == 0)
+                {
+                    ResultsBox.Text = "cannot divide by zero";
+                }
+                else
+                {
+                    result = firstNum / secondNum;
+                    ResultsBox.Text = result.ToString();
+                    return;
+                }
+                
+            }
+            //Muliply
+            else if (function == '*')
+            {
+                result = firstNum * secondNum;
+                ResultsBox.Text = result.ToString();
+            }
+
+
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-            function = 'c';
+            first = "";
+            second = "";
+            userInput = "";
+            result = 0.0;
+            ResultsBox.Text = "0";
         }
 
         private void PlusButton_Click(object sender, EventArgs e)
         {
             function = '+';
+            first = userInput;
+            userInput = "";
         }
 
         private void MinusButton_Click(object sender, EventArgs e)
         {
             function = '-';
+            first = userInput;
+            userInput = "";
         }
 
         private void TimesButton_Click(object sender, EventArgs e)
         {
             function = '*';
+            first = userInput;
+            userInput = "";
         }
 
         private void DivideButton_Click(object sender, EventArgs e)
         {
             function = '/';
+            first = userInput;
+            userInput = "";
         }
 
         private void Num9_Click(object sender, EventArgs e)
         {
-
+            ResultsBox.Text = "";
+            userInput += "9";
+            ResultsBox.Text += userInput;
         }
     }
 }
